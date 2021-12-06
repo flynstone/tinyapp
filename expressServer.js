14b4a5589b52b341a45d6a43905d633721808d5f
Julien <flynstone@live.ca>
2021-12-06 15:26:27 -0500

    commit updates include rename of express_server

--- expressServer.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
-
-app.set("view engine", "ejs");
---
+++ express_server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.set("view engine", "ejs");
